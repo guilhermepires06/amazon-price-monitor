300060462afaa2669c64b37b0712739ea0ac481b
Guilherme Fernando Pires <75048254+guilhermepires06@users.noreply.github.com>
2025-11-14 22:46:24 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -457,7 +457,7 @@ st.markdown(
     padding: 0.9rem 1rem 1.2rem 1rem;
 
     /* --- O QUE FOI ALTERADO --- */
-    max-width: 1150px !important;    /* 30% mais largo */
+    max-width: 9150px !important;    /* 30% mais largo */
     width: 100%;
     min-height: 420px;              /* pouquinho mais alto p/ gráfico */
     /* -------------------------- */
